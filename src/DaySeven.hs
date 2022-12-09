f8fc1ec544419f1b92b022127ab5e9cb4fbf45c8
aiwaverse <aiwavision@protonmail.com>
2022-12-09 10:06:43 -0300

    Finished day 7 (FINALLY)

--- src/DaySeven.hs
+++ src/DaySeven.hs
@@ -1,34 +1,55 @@
 module DaySeven where
 
 import Data.Foldable (Foldable (foldl'))
-import Data.Map (Map, insert, insertWith, empty)
+import Data.List (sort)
+import qualified Data.Map as M
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
-import Data.List (intersperse)
 import Debug.Trace
 import System.Environment (getArgs)
 
-type Directories = Map T.Text Integer
+type Directories = M.Map T.Text Integer
 
 type Path = [T.Text]
+
+processInputDay7 :: IO (String, String)
+processInputDay7 = do
+  args <- getArgs
+  content <- T.lines <$> T.readFile (head args)
+  let folders = sumFolders $ processSystemLine content [] M.empty
+  let sumOfSizes = sum . M.elems . M.filter (<= 100000) $ folders
+  let freeSpace = 70000000 - M.findWithDefault 0 "/" folders
+  let neededSpace = 30000000 - freeSpace
+  print neededSpace
+  let bestFolderToDelete = minimum . M.elems . M.filter (>= neededSpace) $ folders
+  pure (show sumOfSizes, show bestFolderToDelete)
 
 processSystemLine :: [T.Text] -> Path -> Directories -> Directories
 processSystemLine [] _ folders = folders
 processSystemLine (x : xs) path folders = case T.words x of
-  ["$", "cd", ".."] -> processSystemLine xs (dropLast path) folders
-  ["$", "cd", dirName] -> processSystemLine xs (path ++ ["/", dirName]) folders
-  ["dir", dirName] -> processSystemLine xs path (insert (mconcat $ path ++ ["/", dirName]) 0 folders)
-  ["$", "ls", _] -> processSystemLine xs path folders
-  [T.unpack -> size, _] -> trace size $
-    processSystemLine xs path (insertWith (+) (mconcat path) (read size) folders)
+  ["$", "cd", ".."] -> processSystemLine xs (dropTwoLast path) folders
+  ["$", "cd", dirName] ->
+    processSystemLine
+      xs
+      (path ++ (if dirName == "/" then [dirName] else [dirName, "/"]))
+      folders
+  ["dir", dirName] -> processSystemLine xs path (M.insert (mconcat $ path ++ [dirName, "/"]) 0 folders)
+  ["$", "ls"] -> processSystemLine xs path folders
+  [size, _] ->
+    processSystemLine xs path (M.insertWith (+) (mconcat path) (read $ T.unpack size) folders)
   _ -> processSystemLine xs path folders
 
-dropLast :: [a] -> [a]
-dropLast [] = []
-dropLast [_] = []
-dropLast (x : xs) = x : dropLast xs
+sumFolders :: Directories -> Directories
+sumFolders m = M.mapWithKey (\key val -> (+ val) . sum . M.elems . M.filterWithKey (\k _ -> key `T.isPrefixOf` k && key /= k) $ m) m
+
+dropTwoLast :: [a] -> [a]
+dropTwoLast [] = []
+dropTwoLast [_, _] = []
+dropTwoLast (x : xs) = x : dropTwoLast xs
 
 testIO :: IO ()
 testIO = do
-  content <- drop 1 . T.lines <$> T.readFile "/home/phi/Documents/codes/haskell/advent-of-code/inputs/test.txt"
-  print $ T.words <$> content
+  content <- T.lines <$> T.readFile "/home/phi/Documents/codes/haskell/advent-of-code/inputs/test.txt"
+  let folders = sumFolders $ processSystemLine content [] M.empty
+  let sumOfSizes = sum . M.elems . M.filter (<= 100000) $ folders
+  print sumOfSizes
